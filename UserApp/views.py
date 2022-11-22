4a67827227ea5134baee17a2a517daa6e82c4d53
Amna-Laghari <amna.laghari@devsinc.com>
2022-11-22 23:48:29 +0500

    corrected formattin for files

--- UserApp/views.py
+++ UserApp/views.py
@@ -1,132 +1,177 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib import messages
+from django.contrib import auth, messages
 from django.contrib.auth import authenticate, login, logout
-from blog_app import settings
-from django.core.mail import EmailMessage
+from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes, force_str
-from . tokens import generate_token
-from django.views import View
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.views import View
+from django.views.generic import UpdateView
+
+from blog_app import settings
+
 from . import models
 from .decorators import unauthenticated_user
-from .forms import SignupForm, SigninForm, UpdateUserForm
+from .forms import SigninForm, SignupForm, UpdateUserForm
+from .tokens import generate_token
 from .utils import is_moderator
-from django.views.generic import UpdateView
-from django.urls import reverse_lazy
 
+
+# from lockout.exceptions import LockedOut
 def home(request):
-  if request.user.is_authenticated:
-    if is_moderator(request.user):
-      return redirect('index')
-    return redirect('posts')
-  return render(request, 'UserApp/home.html')
+    if request.user.is_authenticated:
+        if is_moderator(request.user):
+            return redirect('index')
+        return redirect('posts')
+    return render(request, 'UserApp/home.html')
+
 
 def signout(request):
-  logout(request)
-  messages.success(request, "you have logged out successfully")
-  return redirect('home')
+    logout(request)
+    messages.success(request, "you have logged out successfully")
+    return redirect('home')
+
 
 def activate(request, uidb64, token):
-  try:
-    uid = force_str(urlsafe_base64_decode(uidb64))
-    myuser = models.CustomUser.objects.get(pk = uid)
-  except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-    myuser = None
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        myuser = models.CustomUser.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
 
-  if myuser is not None and generate_token.check_token(myuser, token):
-    myuser.is_active = True
-    myuser.save()
-    return redirect('home')
-  else:
-    return render(request, 'activation_failed.html')
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        return redirect('home')
+    else:
+        return render(request, 'activation_failed.html')
+
 
 class Signup(View):
-  def get(self, request):
-    context = {'form': SignupForm()}
-    return render(request, 'UserApp/signup.html', context)
+    def check_user(self, form_username):
+        user_length = models.CustomUser.objects.filter(
+            username=form_username).count()
+        if user_length > 0:
+            messages.error(self.request, 'Username already exists')
+            return False
+        return True
 
-  def post(self,request):
-    username  = request.POST.get('username')
-    firstname  = request.POST.get('first_name')
-    lastname  = request.POST.get('last_name')
-    email  = request.POST.get('email')
-    password  = request.POST.get('password')
-    confirm_password  = request.POST.get('confirm_password')
-    group = request.POST.get('groups')
+    def check_password(self, password, confirm_password):
+        if len(password) < 6:
+            messages.error(
+                self.request, 'Password must be at least 6 characters')
+            return False
+        else:
+            if password != confirm_password:
+                messages.error(
+                    self.request, 'Password does not match confirm_password')
+                return False
+            return True
 
-    if password != confirm_password:
-      messages.error(request,'Passowrd didnt match')
-      return redirect('home')
+    def get(self, request):
+        context = {'form': SignupForm()}
+        return render(request, 'UserApp/signup.html', context)
 
-    myuser = models.CustomUser.objects.create_user(username=username, email=email, password=password, first_name = firstname, last_name = lastname)
-    myuser.groups.set(group)
-    myuser.is_active = False
-    myuser.save()
+    def post(self, request):
+        username = request.POST.get('username')
+        firstname = request.POST.get('first_name')
+        lastname = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        group = request.POST.get('groups')
 
-    current_site = get_current_site(request)
-    subject = "Confirmation email"
-    message = render_to_string('confirmation_email.html',{
-      'name': myuser.username,
-      'domain': current_site.domain,
-      'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
-      'token': generate_token.make_token(myuser)
-    })
-    email = EmailMessage(subject, message, settings.EMAIL_HOST_USER, [myuser.email])
-    email.fail_silently = True
-    email.send()
+        if password != confirm_password:
+            messages.error(request, 'Passowrd didnt match')
+            return redirect('home')
+        email_check = self.check_user(username)
+        password_check = self.check_password(password, confirm_password)
 
-    messages.success(request, "Your account has been successfully created")
-    return redirect('signin')
+        if email_check and password_check:
+            myuser = models.CustomUser.objects.create_user(
+                username=username, email=email, password=password, first_name=firstname, last_name=lastname)
+            myuser.groups.set(group)
+            myuser.is_active = False
+            myuser.save()
+
+            current_site = get_current_site(request)
+            subject = "Confirmation email"
+            message = render_to_string('confirmation_email.html', {
+                'name': myuser.username,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+                'token': generate_token.make_token(myuser)
+            })
+            email = EmailMessage(
+                subject, message, settings.EMAIL_HOST_USER, [myuser.email])
+            email.fail_silently = True
+            email.send()
+
+            messages.success(
+                request, "Your account has been successfully created")
+            return redirect('signin')
+        else:
+            return redirect('signup')
+
 
 @method_decorator(unauthenticated_user, name='dispatch')
 class Signin(View):
 
-  def get(self, request):
-    context = {'form': SigninForm()}
-    return render(request, 'UserApp/signin.html', context)
+    def get(self, request):
+        context = {'form': SigninForm()}
+        return render(request, 'UserApp/signin.html', context)
 
-  def post(self, request):
-    username  = request.POST.get('username')
-    password = request.POST.get('password')
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-    user = authenticate(request, username = username, password = password)
+        # try:
+        #     user = auth.authenticate(username=username, password=password)
+        # except LockedOut:
+        #     messages.error(
+        #         request, 'Your account has been locked out because of too many failed login attempts.')
 
-    if user is not None:
-      login(request,user)
-      messages.success(request, "You have logged in successfully")
-      if is_moderator(request.user):
-        return redirect('index')
-      return redirect('posts')
-    else:
-      messages.error(request, "Bad credentials")
-      return redirect('home')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have logged in successfully")
+            if is_moderator(request.user):
+                return redirect('index')
+            return redirect('posts')
+        else:
+            messages.error(request, "Bad credentials")
+            return redirect('home')
+
 
 class UpdateUser(UpdateView):
-  form_class = UpdateUserForm
-  template_name= 'UserApp/edit.html'
-  success_url= reverse_lazy('posts')
+    form_class = UpdateUserForm
+    template_name = 'UserApp/edit.html'
+    success_url = reverse_lazy('posts')
 
-  def get_object(self):
-    return self.request.user
+    def get_object(self):
+        return self.request.user
 
-  def form_valid(self, form):
-    myuser = self.request.user
-    myuser.is_active = False
-    logout(self.request)
-    messages.success(self.request, 'User updated successfully kindly login again')
-    current_site = get_current_site(self.request)
-    subject = "Confirmation email"
-    message = render_to_string('confirmation_email.html',{
-      'name': myuser.username,
-      'domain': current_site.domain,
-      'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
-      'token': generate_token.make_token(myuser)
-    })
-    email = EmailMessage(subject, message, settings.EMAIL_HOST_USER, [myuser.email])
-    email.fail_silently = True
-    email.send()
-    return super(UpdateUser, self).form_valid(form)
+    def form_valid(self, form):
+        myuser = self.request.user
+        myuser.is_active = False
+        logout(self.request)
+        messages.success(
+            self.request, 'User updated successfully kindly login again')
+        current_site = get_current_site(self.request)
+        subject = "Confirmation email"
+        message = render_to_string('confirmation_email.html', {
+            'name': myuser.username,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser)
+        })
+        email = EmailMessage(
+            subject, message, settings.EMAIL_HOST_USER, [myuser.email])
+        email.fail_silently = True
+        email.send()
+        return super(UpdateUser, self).form_valid(form)
